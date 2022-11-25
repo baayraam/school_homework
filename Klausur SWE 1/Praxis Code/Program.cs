4a1f43592ef6cf60108c8e6077314870425a08e5
baayraam <118849606+baayraam@users.noreply.github.com>
2022-11-25 17:26:33 +0100

    SWE Klausur

---
+++ Klausur SWE 1/Praxis Code/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace Klausur_1_Praxis_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int gewürzTausch;
+
+
+                Console.WriteLine("Sie müssen nun einen Gewürzschrank mit Gewürzen füllen die nach der Reihe durchnummeriert sind: \n");
+
+
+                Console.WriteLine("Wie Groß ist Ihr Gewürzschrank, geben Sie eine Zahl ein:");
+                int arrayLänge = zahleneingabe();
+
+
+                int[] Gewürzregal = new int[arrayLänge];
+
+
+                for (int i = 0; i < Gewürzregal.Length; i++)
+                {
+                    Console.WriteLine("Geben Sie das {0}. Gewürz ein: ", (i + 1));
+                    Gewürzregal[i] = zahleneingabe();
+                }
+
+
+
+                ArrayAusgabe(Gewürzregal);
+
+
+                Console.WriteLine("Möchten sie ein Gewürz tauschen? (J/N)");
+                do
+                {
+                    string abfrage = Console.ReadLine();
+
+
+                    if (abfrage == "J" || abfrage == "j")
+                    {
+                        Console.WriteLine("Welches Gewürz wollen Sie ändern?");
+                        gewürzTausch = zahleneingabe();
+
+
+                        Console.WriteLine("Welche Zahl wollen Sie stattdessen tauschen");
+
+
+                        for (int i = 0; i < Gewürzregal.Length; i++)
+                        {
+                            if (gewürzTausch == (i + 1))
+                            {
+                                int neuesGewürz = zahleneingabe();
+                                if (neuesGewürz == Gewürzregal[i])
+                                {
+                                    Console.WriteLine("Die eingegebene Zahl ist die selbe wie davor! Der Tausch wird bestätigt ohne etwas getauscht zu haben!");
+                                }
+                                else
+                                {
+                                    Gewürzregal[i] = neuesGewürz;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    else if (abfrage == "N" || abfrage == "n")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sie haben etwas falsches eingegeben: {0}. Bitte geben Sie (J/N) ein:", abfrage);
+                    }
+                } while (true);
+
+
+                Console.Clear();
+
+
+                Console.WriteLine("Das sind Ihre Gewürze \n ");
+                ArrayAusgabe(Gewürzregal);
+
+
+                Console.WriteLine("Wollen Sie das Programm neu starten? (J/N)");
+                string Programmneustarten = Console.ReadLine();
+
+
+                if (Programmneustarten == "J" || Programmneustarten == "j")
+                {
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Programm wird beendet!");
+                    break;
+                }
+            } while (true);
+        }
+        public static void ArrayAusgabe(int[] Gewürzregal)
+        {
+            for (int i = 0; i < Gewürzregal.Length; i++)
+            {
+                Console.WriteLine("Dein {0}. Gewürz hat die Zahl: {1}", (i + 1), Gewürzregal[i]);
+            }
+        }
+        public static int zahleneingabe()
+        {
+            int zahl;
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out zahl))
+                {
+                    return zahl;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Klausur SWE 1/Theorie Code/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Klausur_1_Theorie_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable für die einzugebenden Zahlenwerte durch den Nutzer
+            double zahl1, zahl2, ergebnis = 0;
+
+            //Variable für das Rechenzeichen 
+            string operand;
+
+            //Aufforderung an den Nutzer eine Zahl einzugeben, welche anschließend in die erste Variable umkonvertiert und gespeichert wird
+            Console.WriteLine("Bitte geben Sie die erste Zahl ein: ");
+            zahl1 = Convert.ToDouble(Console.ReadLine());
+
+            //Aufforderung an den Nutzer eine zweite Zahl einzugeben, welche anschließend in die zweite Variable umkonvertiert und gespeichert wird
+            Console.WriteLine("Bitte geben Sie die zweite Zahl ein: ");
+            zahl2 = Convert.ToDouble(Console.ReadLine());
+
+            //Aufforderung an den Nutzer eine Rechenoperanden einzugeben, welche anschließend in die Variable für das Rechenzeichen gespeichert wird
+            Console.WriteLine("Bitte wählen Sie den Operanden aus: ");
+            operand = Console.ReadLine();
+
+            // Hier kommt der Code für die Berechnung:
+            switch (operand)
+            {
+                //wenn der Benutzer + eingibt also addieren will, dann wird aber Subtrahiert und die Falsche rechnung in der Console ausgegeben
+                case "+":
+                    Console.WriteLine("{0} - {1} = {2}", zahl1, zahl2, ergebnis);
+                    ergebnis = zahl1 - zahl2;
+                    break;
+                //wenn der Benutzer - eingibt also subtrahieren will, dann wird aber dividiert und die Falsche rechnung in der Console ausgegeben
+                case "-":
+                    Console.WriteLine("{0} : {1} = {2}", zahl1, zahl2, ergebnis);
+                    ergebnis = zahl1 / zahl2;
+                    break;
+                //wenn der Benutzer/  eingibt also dividieren will, dann wird aber multipliziert und die Falsche rechnung in der Console ausgegeben
+                case "/ ":
+                    Console.WriteLine("{0} * {1} = {2} ", zahl1, zahl2, ergebnis);
+                    ergebnis = zahl1 * zahl2;
+                    break;
+                //wenn der Benutzer * eingibt also multiplizieren will, dann wird aber addiert und die Falsche rechnung in der Console ausgegeben
+                case "*":
+                    Console.WriteLine("{ 0} + {1} = {2}", zahl1, zahl2, ergebnis);
+                    ergebnis = zahl1 + zahl2;
+                    break;
+
+            }
+        }
+    }
+}
